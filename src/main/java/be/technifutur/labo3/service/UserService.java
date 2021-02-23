d4bea8392f06c1380f69d2bfb425bfb884ea417a
Benjamin Valin <chbench1502@gmail.com>
2021-02-23 15:23:03 +0100

    Modifs UserService
    Méthodes getAll, getById, insert

--- src/main/java/be/technifutur/labo3/service/UserService.java
+++ src/main/java/be/technifutur/labo3/service/UserService.java
@@ -2,23 +2,41 @@ package be.technifutur.labo3.service;
 
 import be.technifutur.labo3.dto.UserDTO;
 import be.technifutur.labo3.entity.User;
+import be.technifutur.labo3.mapper.Mapper;
+import be.technifutur.labo3.repository.UserRepository;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 public class UserService implements Crudable<User, UserDTO, Integer> {
+
+    private final UserRepository userRepository;
+    private final Mapper mapper;
+
+    public UserService(UserRepository userRepository, Mapper mapper) {
+        this.userRepository = userRepository;
+        this.mapper = mapper;
+    }
+
     @Override
     public List<UserDTO> getAll() {
-        return null;
+        return this.userRepository.findAll()
+                .stream()
+                .map(mapper::toUserDTO)
+                .collect(Collectors.toList())
+                ;
     }
 
     @Override
     public UserDTO getById(Integer integer) {
-        return null;
+        return mapper.toUserDTO(userRepository.findById(integer).orElseThrow(() -> new NoSuchElementException()));
     }
 
     @Override
     public boolean insert(User user) {
-        return false;
+        User newUser = this.userRepository.save(user);
+        return this.userRepository.findById(newUser.getId()).isPresent();
     }
 
     @Override
