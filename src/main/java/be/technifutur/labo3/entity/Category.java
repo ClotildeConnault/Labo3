900b8b77309584c4015cca8a340e07f42cbf4b60
Clotilde Connault <clotilde.connault@gmail.com>
2021-02-23 13:48:10 +0100

    product entity - category entity

---
+++ src/main/java/be/technifutur/labo3/entity/Category.java
@@ -0,0 +1,23 @@
+package be.technifutur.labo3.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String name;
+}
