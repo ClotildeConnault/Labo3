d5bef86d926a3cb5ad298129c69b4a2d92c7030b
Clotilde Connault <clotilde.connault@gmail.com>
2021-02-23 16:16:30 +0100

    maj DataInit

--- src/main/java/be/technifutur/labo3/config/DataInit.java
+++ src/main/java/be/technifutur/labo3/config/DataInit.java
@@ -2,6 +2,8 @@ package be.technifutur.labo3.config;
 
 import be.technifutur.labo3.entity.*;
 import be.technifutur.labo3.service.ProductService;
+import be.technifutur.labo3.service.SupplierService;
+import be.technifutur.labo3.service.UserService;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,10 @@ import java.util.List;
 public class DataInit implements InitializingBean {
 
     private final ProductService productService;
+    private final CategoryService categoryService;
+    private final UserService userService;
+    private final SupplierService supplierService;
+
 
     private List<Category> categories = Arrays.asList(
             Category.builder()
@@ -54,12 +60,16 @@ public class DataInit implements InitializingBean {
 
     );
 
-    public DataInit(ProductService productService) {
+    public DataInit(ProductService productService, UserService userService, SupplierService supplierService) {
         this.productService = productService;
+        this.userService = userService;
+        this.supplierService = supplierService;
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
-
+        categories.forEach(categoryService::insert);
+        suppliers.forEach(supplierService::insert);
+        products.forEach(productService::insert);
     }
 }
