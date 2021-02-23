d84c38c07e9650a2975c3cfc80a6b59e859b23c3
Benjamin Valin <chbench1502@gmail.com>
2021-02-23 14:58:10 +0100

    Modif Mapper pour User
    Création des toUserDTO et toUserEntity

--- src/main/java/be/technifutur/labo3/dto/UserDTO.java
+++ src/main/java/be/technifutur/labo3/dto/UserDTO.java
@@ -2,7 +2,15 @@ package be.technifutur.labo3.dto;
 
 import be.technifutur.labo3.entity.AccessLevel;
 import be.technifutur.labo3.entity.Address;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserDTO {
 
     int id;
