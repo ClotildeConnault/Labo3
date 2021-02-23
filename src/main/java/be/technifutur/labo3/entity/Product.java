6b73524ad7b87b42c35585ec506a82601a00bbc5
Clotilde Connault <clotilde.connault@gmail.com>
2021-02-23 13:48:10 +0100

    product entity - category entity

--- src/main/java/be/technifutur/labo3/entity/Product.java
+++ src/main/java/be/technifutur/labo3/entity/Product.java
@@ -1,4 +1,59 @@
 package be.technifutur.labo3.entity;
 
+import com.sun.istack.Nullable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.Instant;
+import java.util.List;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column(nullable = false)
+    private Instant insertDate;
+
+    @Column
+    private Instant updateDate;
+
+    @Column
+    private Instant expirationDate;
+
+    @Column(nullable = false)
+    private double price;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column
+    private String imagePath;
+
+    @Column(nullable = false)
+    private double TVA;
+
+    @ManyToMany
+    private List<Category> categories;
+
+
+
+
 }
