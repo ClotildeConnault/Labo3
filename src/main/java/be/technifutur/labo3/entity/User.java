8eaa9a2f6327351ce0bbd0025a29e4c645dbd9d8
Benjamin Valin <chbench1502@gmail.com>
2021-02-23 12:42:38 +0100

    Test commit entity

--- src/main/java/be/technifutur/labo3/entity/User.java
+++ src/main/java/be/technifutur/labo3/entity/User.java
@@ -1,4 +1,26 @@
 package be.technifutur.labo3.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    
 }
