00568622558d61e43b48c4d6fa38018a3d36e2e3
Benjamin Valin <chbench1502@gmail.com>
2021-04-06 09:32:40 +0200

    Modifs JwtAuthorizationFilter + UserService et Repo

--- src/main/java/be/technifutur/labo3/repository/UserRepository.java
+++ src/main/java/be/technifutur/labo3/repository/UserRepository.java
@@ -3,5 +3,8 @@ package be.technifutur.labo3.repository;
 import be.technifutur.labo3.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
 }
